55d4f7a3661d68b51d95b7db1f17c3a2891ab0ce
Univesp-ErickBento <erickbento.univesp@gmail.com>
2024-01-25 11:26:02 -0300

    Desafio Classe Calculadora

--- src/main/java/br/com/cdb/bancodigital/Main.java
+++ src/main/java/br/com/cdb/bancodigital/Main.java
@@ -1,7 +1,42 @@
 package br.com.cdb.bancodigital;
 
+import br.com.cdb.bancodigital.Calculadora;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int valor1, valor2, resultado = 0;
+        char operacao;
+
+        System.out.println("===============================================");
+        System.out.println("Seja Bem Vindo ao nosso Sistema de Calculadora!");
+        System.out.println("===============================================");
+
+        System.out.println("Digite um Numero");
+        valor1 = input.nextInt();
+
+        System.out.println("Digite outro Numero");
+        valor2 = input.nextInt();
+
+        System.out.println("Qual Operção você quer Realizar?");
+
+        System.out.println("Digite + para Soma");
+        System.out.println("Digite - para Subtração");
+        System.out.println("Digite * para Multiplicação");
+        System.out.println("Digite / para Divisão");
+
+        operacao = input.next().charAt(0);
+
+
+        Calculadora calculadora = new Calculadora();
+        calculadora.calcular(valor1,valor2,operacao);
+
+
+
+        System.out.println(calculadora.getResultado());
+
     }
+
 }
